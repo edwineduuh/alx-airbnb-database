f1a5d8c80f9815e6c325a74f6c532d43808c2e30
edwineduuh <edumwas92@gmail.com>
2025-05-16 04:58:28 +0300

    Implement Indexes for Optimization

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -2,3 +2,6 @@
 CREATE INDEX idx_bookings_user_id ON bookings(user_id);
 CREATE INDEX idx_bookings_property_id ON bookings(property_id);
 CREATE INDEX idx_bookings_booking_date ON bookings(booking_date);
+
+EXPLAIN ANALYZE
+SELECT * FROM bookings WHERE user_id = 123;
