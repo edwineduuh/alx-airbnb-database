a128185790ac7e07629e6b1e55ebcb692da96ca8
edwineduuh <edumwas92@gmail.com>
2025-05-16 04:22:49 +0300

    Include order by to the left join query

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,9 +4,10 @@ FROM bookings
 INNER JOIN users ON bookings.user_id = users.id;
 
 -- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
-SELECT properties.*, reviews.*
+SELECT properties.id, properties.name, reviews.comment
 FROM properties
-LEFT JOIN reviews ON properties.id = reviews.property_id;
+LEFT JOIN reviews ON properties.id = reviews.property_id
+ORDER BY properties.id;
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or the booking is not linked to a user
 SELECT users.*, bookings.*
