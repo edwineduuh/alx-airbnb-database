209a06465440c298216a76341329cda91d047f91
edwineduuh <edumwas92@gmail.com>
2025-05-16 04:46:19 +0300

    Add query to rank properties using row number

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -4,7 +4,16 @@ FROM bookings
 GROUP BY user_id
 ORDER BY total_bookings DESC;
 
--- 2. Rank properties by total bookings using ROW_NUMBER()
+-- 2. Rank properties by total bookings using RANK()
+SELECT
+  property_id,
+  COUNT(*) AS total_bookings,
+  RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+FROM bookings
+GROUP BY property_id
+ORDER BY booking_rank;
+
+-- 3. Rank properties by total bookings using ROW_NUMBER()
 SELECT
   property_id,
   COUNT(*) AS total_bookings,
@@ -12,4 +21,3 @@ SELECT
 FROM bookings
 GROUP BY property_id
 ORDER BY booking_rank;
-
